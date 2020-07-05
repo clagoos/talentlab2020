3fb1684e1a4944c0d92516ed4368cbddbc4fd331
Anonymous <>
2020-07-05 21:04:33 +0200

    trying to push changes from feature branch and pull request

--- TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
+++ TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
@@ -8,6 +8,7 @@ namespace TalentLab2020GitIntro.ConsoleApp
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to git! Anonymously!");
+            Console.WriteLine("I pushed to feature branch and then created a PR (pull request) to have my changes merged to develop branch!");
         }
     }
 }
