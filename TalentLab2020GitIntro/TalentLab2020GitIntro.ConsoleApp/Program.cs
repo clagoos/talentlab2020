cc4f18f7c4d13da1d6b18f4bed6d7dae3e270550
Lechu <osinski.lech@gmail.com>
2020-07-05 22:35:13 +0200

    fixing indentation

--- TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
+++ TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
@@ -9,7 +9,7 @@ namespace TalentLab2020GitIntro.ConsoleApp
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to git! Anonymously!");
             Console.WriteLine("I pushed to feature branch and then created a PR (pull request) to have my changes merged to develop branch!");
-			Console.WriteLine("Pushing from second account to test if students can pull request!")
+            Console.WriteLine("Pushing from second account to test if students can pull request!")
         }
     }
 }
