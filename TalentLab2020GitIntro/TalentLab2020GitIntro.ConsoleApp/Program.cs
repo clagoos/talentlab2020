437c200c2991d3a656ab93d66bd661251e1147b6
Lech Osinski (LCO) <LCO@KMDPoland.pl>
2020-07-05 22:42:19 +0200

    add missing ;

--- TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
+++ TalentLab2020GitIntro/TalentLab2020GitIntro.ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace TalentLab2020GitIntro.ConsoleApp
             Console.WriteLine("Welcome to git! Anonymously!");
             Console.WriteLine("Just adding some chaos here!");
             Console.WriteLine("I pushed to feature branch and then created a PR (pull request) to have my changes merged to develop branch!");
-            Console.WriteLine("Pushing from second account to test if students can pull request!")
+            Console.WriteLine("Pushing from second account to test if students can pull request!");
         }
     }
 }
